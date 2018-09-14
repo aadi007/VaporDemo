bb1001cbf959d0219b373ada2413c2e1a0777cc0
Aadesh Maheshwari <amaheshwari@Aadeshs-mac.local>
2018-09-14 13:10:40 +0400

    added all files

---
+++ APIExample/Sources/App/Controllers/UsersViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UsersViewController.swift
+//  APIExamplePackageDescription
+//
+//  Created by Aadesh Maheshwari on 5/19/18.
+//
+
+import Vapor
+
+final class UsersViewController {
+    func greet(_ req: Request) throws -> String {
+        return "Fetchinng Users..."
+    }
+}
